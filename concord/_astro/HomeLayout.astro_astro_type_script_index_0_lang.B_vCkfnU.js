1e490be74e297b21662553601ab932df32ae824f
Oleg Ivanov <heavenvip@aol.com>
2026-09-24 17:09:17 -0400

    feat: publish responsive and template provenance improvements

---
+++ concord/_astro/HomeLayout.astro_astro_type_script_index_0_lang.B_vCkfnU.js
@@ -0,0 +1,1 @@
+import"./BaseLayout.astro_astro_type_script_index_0_lang.CrtnUJKm.js";import"./prefix.Bg93rmZE.js";import"./input.DiXHCJWi.js";import"./tooltip.CDofL-Fy.js";
--- concord/_astro/HomeLayout.astro_astro_type_script_index_0_lang.N0iDAQ4D.js
+++
@@ -1,1 +0,0 @@
-import"./BaseLayout.astro_astro_type_script_index_0_lang.98idjUE1.js";import"./prefix.Bg93rmZE.js";import"./input.DiXHCJWi.js";import"./tooltip.CDofL-Fy.js";
